fea5418509f1f26627293617e9694276448506af
spec2e <speconsult@gmail.com>
2016-10-30 22:47:16 +0100

    done

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -26,13 +26,7 @@ public class Test2 extends BaseTest {
     @Test
     public void test4() {
         String res = LookAndSay("221", 2);
-        assertEquals("2211", res);
-    }
-
-    @Test
-    public void test5() {
-        String res = LookAndSay("11", 2);
-        assertEquals("2211", res);
+        assertEquals("2221", res);
     }
 
     static String LookAndSay(String input, int n) {
